26f5460f2dfd1c2b62bd71fbf59bd61e45593a21
Ivan Pyslar <ivan.pyslar@additiv.ch>
2022-06-12 22:50:36 +0200

    Update

--- tasks/watch.js
+++ tasks/watch.js
@@ -10,7 +10,7 @@ module.exports = function watching() {
   watch('src/**/*.scss', parallel('style'));
   watch('src/**/*.js', parallel('dev_js'));
   watch('src/img/**/*.+(svg|ico)', parallel('rastr'));
-  watch('src/img/**/*.+(png|jpg|jpeg|gif)', series('rastr', 'webp'));
+  watch('src/img/**/*.+(png|jpg|jpeg|gif|webp)', series('rastr', 'webp'));
   watch('src/svg/css/**/*.svg', series('svg_css', 'style'));
   watch('src/svg/**/*.svg', series('svg_sprite', 'rastr'));
   watch('src/fonts/**/*.ttf', series('ttf', 'ttf2', 'fonts'));
--- tasks/webp.js
+++ tasks/webp.js
@@ -7,7 +7,7 @@ const multiDest = require('gulp-multi-dest');
 const plumber = require('gulp-plumber');
 
 module.exports = function webp() {
-	return src('build/img/**/*.+(png|jpg|jpeg)')
+	return src('build/img/**/*.+(png|jpg|jpeg|gif|webp)')
 		.pipe(plumber())
 		.pipe(changed('build/img', {
 			extension: '.webp'
